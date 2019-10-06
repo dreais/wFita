54ceb26ed2c5aad93b1ae5960a75bb053a0d7da5
dreais <valentin.lattay@gmail.com>
2019-10-06 11:48:57 +0200

    adding pdcurses dependence for windows dev

--- Sources/main.c
+++ Sources/main.c
@@ -1,130 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #ifdef _WIN32
 #include <windows.h>
+#include "../Header/curses.h"
+#else
+#include <ncurses.h>
+#include <sys/ioctl.h>
 #endif
 
 #include "../Header/room.h"
 #include "../Header/shape.h"
 #include "../Header/contourn.h"
 
-void print_cell(const room_t room, unsigned int y, unsigned int x, const line_t line)
-{
-#ifdef _WIN32
-    HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    int color;
-#endif
-    for (unsigned int i = 0; i < line.size; i++) {
-        if (line.pool[i].x == (int)x && line.pool[i].y == (int)y) {
-#ifdef unix
-            printf("\033[0;32m%c\033[0m ", room.room[y][x]);
-#else
-            color = 2;
-            SetConsoleTextAttribute(hConsole, color);
-            printf("%c", room.room[y][x]);
-#endif
-            return;
-        }
-    }
-    if (room.room[y][x] == OBST) {
-#ifdef unix
-        printf("\033[0;31m%c\033[0m ", room.room[y][x]);
-    } else
-        printf("%c ", room.room[y][x]);
-#else
-        color = 12;
-        SetConsoleTextAttribute(hConsole, color);
-        printf("%c", room.room[y][x]);
-    } else {
-        color = 15;
-        SetConsoleTextAttribute(hConsole, color);
-        printf("%c", room.room[y][x]);
-    }
-#endif
-
-}
-
-void print_room(const room_t room, const line_t line)
-{
-    for (unsigned int i = 0; i < room.height; i++) {
-        for (unsigned j = 0; j < room.width; j++) {
-            print_cell(room, i, j, line);
-        }
-        printf("\n");
-    }
-}
-
 int main(void)
 {
 #ifdef _WIN32
-    HANDLE  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    HWND Console = GetConsoleWindow();
+    SetWindowPos(Console, HWND_TOP, 0, 0, GetSystemMetrics(SM_CXSCREEN) - 10, GetSystemMetrics(SM_CYSCREEN) - 10, SWP_SHOWWINDOW);
     int color;
 #endif
     point_t start = { .x = 4, .y = 14 };
     point_t finish = { .x = 11, .y = 0 };
-	room_t room = initialize_room(300, 300);
+    point_t cursor_pos = {.x = 0, .y = 0};
+    room_t room = initialize_room(300, 300);
 	line_t line;
+    char c = 0;
 
+    WINDOW *window = initscr();
     line = create_path(start, finish, room);
     for (int i = 0; i < 65; i++) {
         for (int j = 0, noise; j < 230; j++) {
             noise = room.room[i][j] - 48;
+            move(i, j);
             if (noise > 0 && noise < 3) {
-#ifdef _WIN32
-                color = 2;
-                SetConsoleTextAttribute(hConsole, color);
-#endif
-                printf(".");
+                printw(".");
             } else if (noise >= 3 && noise < 5) {
-#ifdef _WIN32
-                color = 6;
-                SetConsoleTextAttribute(hConsole, color);
-#endif
-                printf(",");
+                printw(",");
             } else if (noise >= 5 && noise < 7) {
-#ifdef _WIN32
-                color = 8;
-                SetConsoleTextAttribute(hConsole, color);
-#endif
-                printf(";");
+                printw(";");
             } else {
-#ifdef _WIN32
-                color = 16;
-                SetConsoleTextAttribute(hConsole, color);
-#endif
-                printf("%%");
+                printw("%%");
             }
         }
-         printf("\n");
     }
-    // TODO proper freeing
+    move(0, 0);
     for (int i = 0; i < room.width; i++)
         free(room.room[i]);
     free(room.room);
     free(line.pool);
     free_hash();
-    getchar();
+    getch();
+    endwin();
     return 0;
 }
 
 /**
-	line = create_path(start, finish, room);
-
-
 putting here codes that is removed to try earlier parts of the program:
-    for (int y = 0; y < 65; y++) {
-        for (int x = 0, noise; x < 225; x++) {
-            noise = (int) (perlin2d(x, y, 0.1f, 4)/10);
-            if (noise > 0 && noise < 3)
-                printf(".");
-            else if (noise >= 3 && noise < 5)
-                printf(",");
-            else if (noise >= 5 && noise < 7)
-                printf(";");
-            else
-                printf("%%");
-        }
-        printf("\n");
-    }
  */
