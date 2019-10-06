1f14b4983001c197ae14468c0a1373a22f9b211f
dreais <valentin.lattay@gmail.com>
2019-10-06 16:34:44 +0200

    see #14

--- Header/print.h
+++ Header/print.h
@@ -17,7 +17,6 @@
 #define GREEN 1
 #define YELLOW 2
 #define WHITE 3
-
 #define DARK_GREEN 4
 #define LIGHT_GREEN 5
 #define GREY 6
@@ -25,6 +24,9 @@
 #define COLOR_DARK_GREEN 22
 #define COLOR_LIGHT_GREEN 119
 #define COLOR_GREY 253
+/**
+ * END OF COLORS DEFINITION
+ */
 
 void print_room(const room_t room, WINDOW *main_game, const point_t player_position, point_t *camera);
 
